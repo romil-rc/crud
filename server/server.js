49c1b04e1db5435653e2fb812910be4c42af522b
romil-rc <romil.chimankar@gmail.com>
2022-12-02 19:10:03 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const dotenv = require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
-const dbURI = process.env.DB_URI;
+const port = process.env.PORT || 5000;
+const dbURI = process.env.DB_URI || `mongodb+srv://saahil:1xWgj1vbKuoEnGH1@cluster0.hhppdti.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(dbURI, (err)=>{
     if(err) console.log(err);
@@ -15,8 +15,12 @@ mongoose.connect(dbURI, (err)=>{
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/notes', require('./routes/notes'));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static("../client/build"));
+}
+
 
 app.listen(port, ()=>{
     console.log(`Connected to port.`);
