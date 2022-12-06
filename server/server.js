1ddd83fd721acb76f51aa936fe558390c104e009
romil-rc <romil.chimankar@gmail.com>
2022-12-06 13:33:22 +0530

    build three

--- server/server.js
+++ server/server.js
@@ -25,9 +25,9 @@ __dirname = path.resolve();
 console.log(__dirname);
 if(process.env.NODE_ENV==="production"){
     console.log("inside production");
-    app.use(express.static(path.join(__dirname, "/client/build")));
+    app.use(express.static(path.join(__dirname, "/client", "build")));
     app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "/client", "build", "index.html"));
     })
 } else {
     app.get('/', (req, res)=>{
