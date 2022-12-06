e34fceb1a83ca5a440e5bb7f0fa3b51fa1c0594e
romil-rc <romil.chimankar@gmail.com>
2022-12-06 18:46:48 +0530

    dotenv comm

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv').config();
+// const dotenv = require('dotenv').config();
 const path = require('path');
 
 const app = express();
